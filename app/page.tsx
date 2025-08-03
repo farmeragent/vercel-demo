8ed7175a0e64c73b8a65e0632dbfe8b667f2231f
Ojas Mhetar <ojmhetar@Ojass-MacBook-Air.local>
2025-08-03 11:29:17 -0700

    Change homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,12 @@ import styles from "./page.module.css";
 const Home = () => {
   const categories = {
     "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
   };
 
   return (
     <main className={styles.main}>
       <div className={styles.title}>
-        Explore sample apps built with Assistants API
+         Farmer Agent Home
       </div>
       <div className={styles.container}>
         {Object.entries(categories).map(([name, url]) => (
